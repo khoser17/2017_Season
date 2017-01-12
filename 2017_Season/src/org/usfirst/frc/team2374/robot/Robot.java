9bab293696d8966f650e0b1555f95ef1326e014e
khoser16 <kompute101@gmail.com>
2017-01-11 19:47:45 -0800

    Add Drivetrain and DriveWithJoystick

--- 2017_Season/src/org/usfirst/frc/team2374/robot/Robot.java
+++ 2017_Season/src/org/usfirst/frc/team2374/robot/Robot.java
@@ -8,8 +8,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team2374.robot.commands.ExampleCommand;
-import org.usfirst.frc.team2374.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team2374.robot.subsystems.Drivetrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,7 +19,7 @@ import org.usfirst.frc.team2374.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static Drivetrain drivetrain = new Drivetrain();
 	public static OI oi;
 
 	Command autonomousCommand;
@@ -33,7 +32,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
+		chooser.addDefault("Default Auto", null);
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
 	}
--- 2017_Season/src/org/usfirst/frc/team2374/robot/subsystems/Drivetrain.java
+++ 2017_Season/src/org/usfirst/frc/team2374/robot/subsystems/Drivetrain.java
@@ -1,30 +1,52 @@
 package org.usfirst.frc.team2374.robot.subsystems;
 
+import org.usfirst.frc.team2374.robot.RobotMap;
+import org.usfirst.frc.team2374.robot.commands.DriveWithJoystick;
+
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drivetrain extends Subsystem {
 	
-	SpeedController fLeft, fRight, mLeft, mRight, bLeft, bRight;
+	CANTalon masterLeft, masterRight, fLeft, fRight, bLeft, bRight;
 	RobotDrive robotDrive;
+	AHRS navX;
 
-	
 	public Drivetrain() {
+		masterLeft = new CANTalon(RobotMap.talonMasterLeft);
+		masterRight = new CANTalon(RobotMap.talonMasterRight);
+		fLeft = new CANTalon(RobotMap.talonFrontLeft);
+		fRight = new CANTalon(RobotMap.talonFrontRight);
+		bLeft = new CANTalon(RobotMap.talonBackLeft);
+		bRight = new CANTalon(RobotMap.talonBackRight);		
+
+		fLeft.changeControlMode(TalonControlMode.Follower);
+		fRight.changeControlMode(TalonControlMode.Follower);
+		bLeft.changeControlMode(TalonControlMode.Follower);
+		bRight.changeControlMode(TalonControlMode.Follower);
+		fLeft.set(0);
+		fRight.set(0);
+		bLeft.set(1);
+		bRight.set(1);
 		
-		fLeft = new TalonSRX(0);
-		fRight = new TalonSRX(1);
-		mLeft = new TalonSRX(2);
-		mRight = new TalonSRX(3);
-		bLeft = new TalonSRX(4);
-		bRight = new TalonSRX(5);
+		masterLeft.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		masterRight.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
 		
-		
-		
+		robotDrive = new RobotDrive(masterLeft, masterRight);
 	}
 
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-
+		setDefaultCommand(new DriveWithJoystick());
+	}
+	
+	public void tankDrive(double left, double right) {
+		robotDrive.tankDrive(left, right);
 	}
 
 }
