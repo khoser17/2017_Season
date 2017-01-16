9608a0acfb5aa2a540a7e2297aba75d618b1fd2f
2374 <crusaderbots@gmail.com>
2017-01-16 11:49:13 -0800

    Add GearManipulator

---
+++ 2017_Season/src/org/usfirst/frc/team2374/robot/subsystems/GearManipulator.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team2374.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class GearManipulator extends Subsystem {
+
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
